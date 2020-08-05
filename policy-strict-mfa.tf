ec81e5a594eedc517a1d4f659ab98f2caf3a14b7
Krzysztof Kluska <krzysztof.kluska@miquido.com>
2020-08-05 13:00:08 +0200

    fix(strict): removed not needed permissions when using strict mode
    strict mfa mode allowed for to many permissions without MFA. Removed not needed permissions

--- policy-strict-mfa.tf
+++ policy-strict-mfa.tf
@@ -8,76 +8,11 @@ data "aws_iam_policy_document" "strict-mfa" {
     sid    = "AllowViewAccountInfo"
     effect = "Allow"
     actions = [
-      "iam:ListUsers",
       "iam:GetAccountPasswordPolicy",
-      "iam:GetAccountSummary",
+      "iam:ListUsers",
       "iam:ListVirtualMFADevices",
     ]
     resources = ["*"]
-  }
-
-  statement {
-    sid    = "AllowManageOwnPasswords"
-    effect = "Allow"
-    actions = [
-      "iam:ChangePassword",
-      "iam:GetUser",
-      "iam:CreateLoginProfile",
-      "iam:DeleteLoginProfile",
-      "iam:GetLoginProfile",
-      "iam:UpdateLoginProfile",
-    ]
-    resources = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/$${aws:username}"]
-  }
-
-  statement {
-    sid    = "AllowManageOwnAccessKeys"
-    effect = "Allow"
-    actions = [
-      "iam:CreateAccessKey",
-      "iam:DeleteAccessKey",
-      "iam:ListAccessKeys",
-      "iam:UpdateAccessKey",
-    ]
-    resources = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/$${aws:username}"]
-  }
-
-  statement {
-    sid    = "AllowManageOwnSigningCertificates"
-    effect = "Allow"
-    actions = [
-      "iam:DeleteSigningCertificate",
-      "iam:ListSigningCertificates",
-      "iam:UpdateSigningCertificate",
-      "iam:UploadSigningCertificate",
-    ]
-    resources = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/$${aws:username}"]
-  }
-
-  statement {
-    sid    = "AllowManageOwnSSHPublicKeys"
-    effect = "Allow"
-    actions = [
-      "iam:DeleteSSHPublicKey",
-      "iam:GetSSHPublicKey",
-      "iam:ListSSHPublicKeys",
-      "iam:UpdateSSHPublicKey",
-      "iam:UploadSSHPublicKey",
-    ]
-    resources = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/$${aws:username}"]
-  }
-
-  statement {
-    sid    = "AllowManageOwnGitCredentials"
-    effect = "Allow"
-    actions = [
-      "iam:CreateServiceSpecificCredential",
-      "iam:DeleteServiceSpecificCredential",
-      "iam:ListServiceSpecificCredentials",
-      "iam:ResetServiceSpecificCredential",
-      "iam:UpdateServiceSpecificCredential",
-    ]
-    resources = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/$${aws:username}"]
   }
 
   statement {
@@ -94,8 +29,9 @@ data "aws_iam_policy_document" "strict-mfa" {
     sid    = "AllowManageOwnUserMFA"
     effect = "Allow"
     actions = [
-      "iam:DeactivateMFADevice",
+      "iam:ChangePassword",
       "iam:EnableMFADevice",
+      "iam:GetUser",
       "iam:ListMFADevices",
       "iam:ResyncMFADevice",
     ]
@@ -103,17 +39,36 @@ data "aws_iam_policy_document" "strict-mfa" {
   }
 
   statement {
+    sid    = "AllowIndividualUserToDeactivateOnlyTheirOwnMFAOnlyWhenUsingMFA"
+    effect = "Allow"
+    actions = [
+      "iam:DeactivateMFADevice"
+    ]
+    resources = [
+      "arn:aws:iam::${data.aws_caller_identity.current.account_id}:mfa/$${aws:username}",
+      "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/$${aws:username}",
+    ]
+
+    condition {
+      test     = "Bool"
+      variable = "aws:MultiFactorAuthPresent"
+      values   = ["true"]
+    }
+  }
+
+  statement {
     sid    = "DenyAllExceptListedIfNoMFA"
     effect = "Deny"
     not_actions = [
       "iam:ChangePassword",
-      "iam:DeleteVirtualMFADevice",
-      "iam:CreateLoginProfile",
-      "iam:ListUsers",
       "iam:CreateVirtualMFADevice",
+      "iam:DeactivateMFADevice",
+      "iam:DeleteVirtualMFADevice",
       "iam:EnableMFADevice",
+      "iam:GetAccountPasswordPolicy",
       "iam:GetUser",
       "iam:ListMFADevices",
+      "iam:ListUsers",
       "iam:ListVirtualMFADevices",
       "iam:ResyncMFADevice",
       "sts:GetSessionToken",
